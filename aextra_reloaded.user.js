dbc2aa11d07121bfcc473654525e6b675c590fec
Pirate505 <Pirate505@users.noreply.github.com>
2018-06-17 13:17:50 +0300

    Update aextra_reloaded.user.js
    -новый домен
    -добавлен сторейдж для конфига
    -сервисные сообщения не работают на грисманки (пока)

--- aextra_reloaded.user.js
+++ aextra_reloaded.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name      AnimachXtra Reloaded
 // @namespace AExtra
-// @version   1.3.3.7
+// @version   1.4.3.7
 // @grant     unsafeWindow
-// @include   http://tehtube.tv/*
-// @include   https://tehtube.tv/*
+// @include   http://animach.com/*
+// @include   https://animach.com/*
 // @require   http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @run-at    document-end
 // ==/UserScript==
@@ -14,8 +14,11 @@
   + изменение фона сайта
   + реконнект
   - команды (?)
+  - сохранение фона
+  - фон панели убрать, поправить стиль
+  - сервисные сообщения
 */
-var ver = '1.3.3.7',
+var ver = '1.3.3.8',
     window = unsafeWindow;
 
 const style = `
@@ -25,19 +28,45 @@ const style = `
     margin-left: 0px !important;
     padding: 5px 5px !important;
     max-height: 30px;
+    border-radius: 0px;
 }
-#xtra-panel {
+.btn-toggled {
+    background: #090c0f;
+    color: #fff;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+    box-shadow: 0 3px #fff;
+    outline: 0;
+}
+.x-panel {
     display: inline-block;
     min-width: 1px;
     max-width: 250px;
     margin-left: 5px;
+    margin-top: 1px;
 }
 .form-xtra {
     max-width: 100px;
-    max-height: 29px;
+    max-height: 31px;
     padding: 5px 10px;
 }`;
 
+function Storage() {
+    if (window.localStorage['rld_conf'] === undefined) {
+        localStorage['rld_conf'] = JSON.stringify({'backgroung': '', 'service_msgs': false})
+    }
+    let config = JSON.parse(localStorage['rld_conf'])
+    this.get = function(prop) {
+        return config[prop];
+    }
+    this.set = function(prop, value) {
+        config[prop] = value;
+        localStorage['rld_conf'] = JSON.stringify(config);
+        return config[prop];
+    }
+}
+
+var storage = new Storage();
 
 function initUI() {
     let btn_class = 'btn btn-sm btn-default btn-xtra';
@@ -54,7 +83,7 @@ function initUI() {
                   .appendTo('#leftcontrols > .btn-group');
   
     $('<div/>').attr('id', 'xtra-panel')
-               .addClass('blur-elem')
+               .addClass('x-panel')
                .appendTo('#leftcontrols > .btn-group');
   
     $('<button/>').attr('id', 'xtra-reconnect')
@@ -62,6 +91,17 @@ function initUI() {
                   .text('🔄')
                   .addClass(btn_class)
                   .click(() => {reconnect(randomInt(500,1000))})
+                  .appendTo('#xtra-panel');
+
+    $('<button/>').attr('id', 'xtra-service')
+                  .attr('title', 'Отображать или скрыть сервисные сообщения')
+                  .text('!!')
+                  .addClass(btn_class)
+                  .click(() => {
+                        $('#xtra-service').hasClass('btn-toggled') ? $('#xtra-service').removeClass('btn-toggled') : $('#xtra-service').addClass('btn-toggled');
+                        let state = $('#xtra-service').hasClass('btn-toggled');
+                        serviceBtn(state);
+                   })
                   .appendTo('#xtra-panel');
 
     $('<button/>').attr('id', 'xtra-clear')
@@ -75,15 +115,21 @@ function initUI() {
                   .attr('title', 'Изменить фон')
                   .text('🏞')
                   .addClass(btn_class)
-                  .click(() => {setBg($('#xtra-bg-file').val())})
+                  .click(() => {setBg($('#xtra-bg-input').val())})
                   .appendTo('#xtra-panel');
 
-    $('<input/>').attr('id', 'xtra-bg-file')
+    $('<input/>').attr('id', 'xtra-bg-input')
                  .attr('title', 'Цветовой код в hex (#abc123) или URL картинки и CSS параметры через пробел')
                  .addClass('form-control form-xtra')
-                 .keydown((ev) => {if (ev.keyCode == 13) setBg($('#xtra-bg-file').val())})
+                 .keydown((ev) => {if (ev.keyCode == 13) setBg($('#xtra-bg-input').val())})
                  .appendTo('#xtra-panel');
     $('#xtra-panel').hide();
+    
+    $('#xtra-bg-input').val(storage.get('background'));
+    $('#xtra-bg').click();
+    if (storage.get('service_msgs')) {
+        $('#xtra-service').click();
+    }
 }
 
 function sendText(text) {
@@ -97,6 +143,70 @@ function msgDelete(user) {
     $('.chat-msg-' + user).remove();
     $('#userlist > :contains(' + user + ')').css('text-decoration', 'line-through');
     window.scrollChat();
+}
+
+function serviceBtn(enable) {
+    if (enable) {
+        window.socket.on('addUser', addUser);
+        window.socket.on('userLeave', userLeave);
+        $('.chat-msg-xtra').show();
+        window.scrollChat();
+    } else {
+        console.log('serviceMsg button clicked with false');
+        removeListener('addUser', addUser);
+        removeListener('userLeave', userLeave);
+        $('.chat-msg-xtra').hide();
+    }
+    storage.set('service_msgs', enable);
+}
+
+function serviceMsg(user, type) {
+    console.log('serviceMsg called');
+    if(user !== '' && user != window.CLIENT.name) {
+        let mdiv = $("<div/>").addClass('chat-msg-xtra');
+
+        if(window.USEROPTS.show_timestamps) {
+            let timestamp = new Date(Date.now()).toTimeString().split(" ")[0];
+            $("<span/>").addClass("timestamp server-whisper")
+                        .text("["+timestamp+"] ")
+                        .appendTo(mdiv);
+                        //.click(function() {$(this).parent().toggle();});
+        }
+        if(type == 'join') {
+            $('<span/>').text(user+" зашел на канал")
+                        .addClass('server-whisper')
+                        .appendTo(mdiv);
+        } else if(type == 'leave') {
+            $('<span/>').text(user+" покинул канал")
+                        .addClass('server-whisper')
+                        .appendTo(mdiv);
+        }
+        return mdiv;
+    } else {
+        return;
+    }
+}
+
+function addUser(data) {
+    console.log('Called addUser listener');
+    let smsg = serviceMsg(data.name, 'join');
+    $(smsg).appendTo("#messagebuffer");
+    //window.LASTCHAT.name = '---';
+    window.scrollChat();
+}
+
+
+function userLeave(data) {
+    console.log('Called addUser listener');
+    let smsg = serviceMsg(data.name, 'leave');
+    $(smsg).appendTo("#messagebuffer");
+    //window.LASTCHAT.name = '---';
+    window.scrollChat();
+}
+
+
+function removeListener(listener, func) {
+    window.socket.removeListener(listener, func);
 }
 
 function randomInt(min, max) {
@@ -117,8 +227,9 @@ function setBg(param) {
         //console.log('url match: '+url);
         body.css('background', 'url("'+url+'") '+params);
     } else if (param.length <= 1) {
-        body.attr('style', '')
+        body.attr('style', '');
     }
+    storage.set('background', param);
 }
 
 function reconnect(time) {
